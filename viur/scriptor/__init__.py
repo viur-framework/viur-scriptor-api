389d707e776b8c2f5a87d1fcc95c416955f0243b
Arne Gudermann <ag@mausbrand.de>
2025-10-16 08:08:51 +0200

    chore: Update Version

--- viur/scriptor/__init__.py
+++ viur/scriptor/__init__.py
@@ -11,7 +11,7 @@ from requests.exceptions import ConnectionError
 from .directory_handler import DirectoryHandler
 from .progressbar import ProgressBar
 
-__version__ = '1.11.0'
+__version__ = '1.12.0'
 
 
 def version():
